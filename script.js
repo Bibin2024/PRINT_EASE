f1f67bc38cc6398802f6ab32ac0e1b3ff7744919
Bibin2024 <83809803+Bibin2024@users.noreply.github.com>
2023-05-25 21:25:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,133 @@
 function displayFileName() {
-    const fileInput = document.getElementById('file-upload');
-    const fileName = document.getElementById('file-name');
-    if (fileInput.files.length > 0) {
-      fileName.textContent = fileInput.files[0].name;
-    } else {
-      fileName.textContent = '';
-    }
-}
-
-var recognition = new webkitSpeechRecognition();
-recognition.lang = 'en-US';
-recognition.onstart = function() {
-  console.log('Recognition started');
-};
-
-recognition.onresult = function(event) {
-  var transcript = event.results[0][0].transcript;
-  var transcriptElement = document.getElementById('transcript');
-  transcriptElement.textContent = transcript;
-};
-
-recognition.onerror = function(event) {
-  console.error('Recognition error:', event.error);
-};
-
-function startRecognition() {
-  recognition.start();
-}
-function stopRecognition() {
-    recognition.stop();
+  const fileInput = document.getElementById('file-upload');
+  const fileName = document.getElementById('file-name');
+  const labelUpload = document.getElementById('label-upload');
+  if (fileInput.files.length > 0) {
+    labelUpload.textContent = fileInput.files[0].name;
+  } else {
+    fileName.textContent = '';
   }
+}
+
+// collect DOMs
+const display = document.querySelector('.display')
+const controllerWrapper = document.querySelector('.controllers')
+
+const State = ['Initial', 'Record', 'Download']
+let stateIndex = 0
+let mediaRecorder, chunks = [], audioURL = ''
+
+// mediaRecorder setup for audio
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    console.log('mediaDevices supported..')
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    }).then(stream => {
+        mediaRecorder = new MediaRecorder(stream)
+
+        mediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data)
+        }
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'})
+            chunks = []
+            audioURL = window.URL.createObjectURL(blob)
+            document.querySelector('audio').src = audioURL
+
+        }
+    }).catch(error => {
+        console.log('Following error has occured : ',error)
+    })
+}else{
+    stateIndex = ''
+    application(stateIndex)
+}
+
+const clearDisplay = () => {
+    display.textContent = ''
+}
+
+const clearControls = () => {
+    controllerWrapper.textContent = ''
+}
+
+const record = () => {
+    stateIndex = 1
+    mediaRecorder.start()
+    application(stateIndex)
+}
+
+const stopRecording = () => {
+    stateIndex = 2
+    mediaRecorder.stop()
+    application(stateIndex)
+}
+
+const downloadAudio = () => {
+    const downloadLink = document.createElement('a')
+    downloadLink.href = audioURL
+    downloadLink.setAttribute('download', 'audio')
+    downloadLink.click()
+}
+
+const addButton = (id, funString, text) => {
+  const btn = document.createElement('button');
+  btn.id = id;
+  btn.setAttribute('onclick', funString);
+  btn.textContent = text;
+  btn.classList.add('addButton');
+
+  controllerWrapper.append(btn);
+};
+
+const addMessage = (text) => {
+    const msg = document.createElement('p')
+    msg.textContent = text
+    display.append(msg)
+}
+
+const addAudio = () => {
+    const audio = document.createElement('audio')
+    audio.controls = true
+    audio.src = audioURL
+    display.append(audio)
+}
+
+const application = (index) => {
+    switch (State[index]) {
+        case 'Initial':
+            clearDisplay()
+            clearControls()
+
+            addButton('record', 'record()', 'Start Recording')
+            break;
+
+        case 'Record':
+            clearDisplay()
+            clearControls()
+
+            addMessage('Recording...')
+            addButton('stop', 'stopRecording()', 'Stop Recording')
+            break
+
+        case 'Download':
+            clearControls()
+            clearDisplay()
+
+            addAudio()
+            addButton('record', 'record()', 'Record Again')
+            break
+
+        default:
+            clearControls()
+            clearDisplay()
+
+            addMessage('Your browser does not support mediaDevices')
+            break;
+    }
+
+}
+
+application(stateIndex)
